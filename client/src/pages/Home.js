714196a152e48c29c6462c9f4604a3d4b0e2e599
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-03-20 01:27:15 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -194,7 +194,7 @@ function Home() {
 					<h2 >featured cars</h2>
 				</div>
                 {loading === true && (<Spinner />)}
-                 {totalCars == 0 && totalCars.map(car => {
+                 {totalCars.map(car => {
 				return <div class="" >
 					<div class="" style={{marign:'10px'}}>
 						<div class="col-lg-3 col-md-4 col-sm-6">
