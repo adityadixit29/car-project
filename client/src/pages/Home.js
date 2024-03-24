df2dc20b7079435bead24fa6337849615fb1423b
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-03-25 01:56:46 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -195,7 +195,7 @@ function Home() {
 					<h2 >featured cars</h2>
 				</div>
                 {loading === true && (<Spinner />)}
-                 {(totalCars) && Object.values(totalCars).map(car => {
+                 {(totalCars) && Object.keys(totalCars).map(car => {
 				return <div class="" >
 					<div class="" style={{marign:'10px'}}>
 						<div class="col-lg-3 col-md-4 col-sm-6">
