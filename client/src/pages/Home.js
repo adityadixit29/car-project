136629a9b10318109f9262d4c6a805f4a55f9233
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-03-25 02:36:17 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -37,38 +37,32 @@ function Home() {
 
         var temp = []
 
-        // for (var car of cars) {
+        for (var car of cars) {
 
-        //     if (car.bookedTimeSlots.length == 0) {
-        //         temp.push(car)
-        //     }
-        //     else {
-
-        //         for (var booking of car.bookedTimeSlots) {
-
-        //             if (selectedFrom.isBetween(booking.from, booking.to) ||
-        //                 selectedTo.isBetween(booking.from, booking.to) ||
-        //                 moment(booking.from).isBetween(selectedFrom, selectedTo) ||
-        //                 moment(booking.to).isBetween(selectedFrom, selectedTo)
-        //             ) {
-
-        //             }
-        //             else {
-        //                 temp.push(car)
-        //             }
-
-        //         }
-
-        //     }
-
-        // }
-
-	for (var car of cars) {
-
-           
+            if (car.bookedTimeSlots.length == 0) {
                 temp.push(car)
-            
-	}
+            }
+            else {
+
+                for (var booking of car.bookedTimeSlots) {
+
+                    if (selectedFrom.isBetween(booking.from, booking.to) ||
+                        selectedTo.isBetween(booking.from, booking.to) ||
+                        moment(booking.from).isBetween(selectedFrom, selectedTo) ||
+                        moment(booking.to).isBetween(selectedFrom, selectedTo)
+                    ) {
+
+                    }
+                    else {
+                        temp.push(car)
+                    }
+
+                }
+
+            }
+
+        }
+
         setTotalCars(temp)
 
 
